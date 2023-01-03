dc18dd6501ff4833f8280951357d63baec03777f
Andrey Povaliy <116582306+AndreyPovaliy@users.noreply.github.com>
2023-01-03 18:33:39 +0300

    New

--- 08.TestList(2_2)/Form1.Designer.cs
+++ 08.TestList(2_2)/Form1.Designer.cs
@@ -100,11 +100,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(45, 194);
+            this.button1.Location = new System.Drawing.Point(38, 194);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 37);
+            this.button1.Size = new System.Drawing.Size(147, 37);
             this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
+            this.button1.Text = "Загрузить данные";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
