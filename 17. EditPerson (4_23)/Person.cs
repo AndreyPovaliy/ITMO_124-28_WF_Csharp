fedddeae7184f273ead5c2e6aad6e8ec3f960029
Andrey Povaliy <116582306+AndreyPovaliy@users.noreply.github.com>
2023-01-10 17:41:05 +0300

    New

---
+++ 17. EditPerson (4_23)/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.EditPerson__4_23_
+
+{
+    
+    public class Person
+    {
+        public string FirstName
+        {
+            get; set;
+        }
+        public string LastName 
+        { 
+            get; set; 
+        }
+        public int Age 
+        { 
+            get; set; 
+        }
+
+        public override string ToString()
+        {
+            return LastName + " " + FirstName + "\nВозраст: " + Age + "\n";
+        }
+    }
+}
---
+++ 19. SinFunc (4_5ctrl)/FormRange.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _19.SinFunc__4_5ctrl_
+{
+    public partial class FormRange : Form
+    {
+        public FormRange()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text + " " + textBox2.Text;
+            string l = "Левая граница: " +textBox1.Text + " правая граница " + textBox2.Text;
+            Form1 frm1 = this.Owner as Form1;
+            if (frm1 != null)
+            {
+                frm1.S_Data = s;
+                frm1.L_Data = l;
+            }
+            this.Close();
+        }
+    }
+    
+}
