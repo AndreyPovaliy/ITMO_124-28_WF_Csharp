d696e9b4146bce0b6cd5849c4a0db7f9fd9d43d5
Andrey Povaliy <116582306+AndreyPovaliy@users.noreply.github.com>
2023-01-12 15:52:09 +0300

    New

---
+++ 20. WinGrafSinLine (6_1)/edit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20.WinGrafSinLine__6_1_
+{
+    public partial class edit : Form
+    {
+        public edit()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 frm1 = this.Owner as Form1;
+
+        }
+    }
+}
